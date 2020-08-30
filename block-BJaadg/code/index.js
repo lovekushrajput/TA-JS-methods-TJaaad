fe621288321287b979af9bde9f27864738c1080f
Ankit <iankit17@gmail.com>
2020-08-30 22:07:39 +0530

    add block-BJaadg

---
+++ block-BJaadg/code/index.js
@@ -0,0 +1,97 @@
+// After knowing about string methods, practice those by solving problems given below.
+
+let from = 'Syrio Forel';
+let quote = 'There is only one thing we say to death: Not today';
+let to = 'Arya Stark';
+
+/*
+1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
+*/
+
+/*
+2. Find the character at, the index indexOfIs (Problem 1) in quote.
+*/
+
+/*
+3. Log the message saying `The index of first is in quote is 7`
+*/
+
+/*
+4. Log the message for first 6 characters of quote like this.
+  The character at index 0 is 'T'
+  The character at index 1 is 'h'
+  The character at index 2 is 'e'
+  The character at index 3 is 'r'
+  The character at index 4 is 'e'
+  The character at index 5 is ' '
+*/
+
+/*
+5. Using the variable from , to and quote variable dispaly this message
+  "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
+*/
+
+/*
+6. Does from, to and quote ends with "rk". Check all three.
+*/
+
+/*
+7. Does quote includes the word "Only"
+*/
+
+/*
+8. Does quote includes the word " we"
+*/
+
+/*
+9. Find the index of the the word `we` in quote
+*/
+
+/*
+10. Split the quote into individual word and store it in a variable name quoteSplitted
+*/
+
+/*
+11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
+*/
+
+/*
+12. Find the index of second "o" in quote. Use indexOf
+*/
+
+/*
+13. Find the last index of letter "a" in quote.
+*/
+
+/*
+14. Find the second last index of letter "a" in quote.
+*/
+
+/*
+15. Make the quote 70 character long. If it has less characters add rest as .......
+Example: "Hello" (convert to 10 characters) => "Hello....."
+*/
+
+/*
+16. Do same as (15) but the ... should come in start.
+*/
+
+/*
+17. Log the repeat of "Hello World!" 10 times.
+*/
+
+/*
+18. Replace today to tomorrow in quote.
+*/
+
+/*
+19. Replace Stark to Lannister in quoteTo
+*/
+
+/*
+20. Make the quote of length 30 and put ... at the end. (use slice)
+*/
+
+/*
+21. Find out does quote, quoteFrom, quoteTo starts with "A"
+*/

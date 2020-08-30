c24590e88c2b6a18bf5c1edf5e0874ca986d8f86
Ankit <iankit17@gmail.com>
2020-08-30 22:07:40 +0530

    add block-BJaadh

---
+++ block-BJaadh/code/index.js
@@ -0,0 +1,62 @@
+// Use the below two arrays and practice array methods
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6, 9];
+var strings = ['This', 'is', 'a', 'collection', 'of', 'words'];
+
+// - Find the index of `101` in numbers
+
+// - Find the last index of `9` in numbers
+
+// - Convert value of strings array into a sentance like "This is a collection of words"
+
+// - Add two new words in the strings array "called" and "sentance"
+
+// - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
+
+// - Remove the first word in the array (strings)
+
+// - Find all the words that contain 'is' use string method 'includes'
+
+// - Find all the words that contain 'is' use string method 'indexOf'
+
+// - Check if all the numbers in numbers array are divisible by three use array method (every)
+
+// -  Sort Array from smallest to largest
+
+// - Remove the last word in strings
+
+// - Find largest number in numbers
+
+// - Find longest string in strings
+
+// - Find all the even numbers
+
+// - Find all the odd numbers
+
+// - Place a new word at the start of the array use (upshift)
+
+// - Make a subset of numbers array [18,9,7,11]
+
+// - Make a subset of strings array ['a','collection']
+
+// - Replace 12 & 18 with 1221 and 1881
+
+// - Replace words in strings array with the length of the word
+
+// - Find the sum of the length of words using above question
+
+// - Customers Array
+var customers = [
+  { firstname: 'Joe', lastname: 'Blogs' },
+  { firstname: 'John', lastname: 'Smith' },
+  { firstname: 'Dave', lastname: 'Jones' },
+  { firstname: 'Jack', lastname: 'White' },
+];
+// - Find all customers whose firstname starts with 'J'
+
+// - Create new array with only first name
+
+// - Create new array with all the full names (ex: "Joe Blogs")
+
+// - Sort the array created above alphabetically
+
+// - Create a new array that contains only user who has at least one vowel in the firstname.

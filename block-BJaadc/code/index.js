2669770ce575237e530926640c9f343ae8aba33f
Ankit <iankit17@gmail.com>
2020-08-30 22:07:39 +0530

    add block-BJaadc

---
+++ block-BJaadc/code/index.js
@@ -0,0 +1,49 @@
+let message = 'If you are still thinking the answer is no!';
+
+// Log the length of the variable message like `The length of [message] is [length]`
+
+// Log the character at index 0 of the variable message like `The first character of [message] is [character]`
+
+// Log the last character of message using length like `The last character of [message] is [character]`
+
+// Log the index of word "you" in message
+
+// Log the index of word "still" in message
+
+// Log the index of word "answers" in message
+
+// Log true or false based on whether the word "answers" exist in message or not
+
+// Log true or false based on whether the word "they" exist in message or not
+
+// Log true or false based on whether the word "is" exist in message or not
+
+// Log true or false based on whether the word "Is" exist in message or not
+
+// Check and log whether the word from index 3 to 6 is "you" or not (use slice)
+
+// Check and log whether the word from index 7 to 8 is "a" or not
+
+// Check and log whether the last 3 character in message is "no!" or not (you can use -3 in slice)
+
+// Check and log whether the last 5 character in message is "no!" or not
+
+// Check and log whether the last 5 character in message is "no!" or not
+
+// Log the message variable, all in lowecase
+
+// Log the message variable, all in uppercase
+
+// Replace the word "are" to "can't", store it in a new variable named `newMessage` and log it.
+
+// Replace the word "still" to "" (empty), and re-assign the output to the variable `newMessage` and log it.
+
+// Replace the word "thinking" to "decide", and re-assign the output to the variable `newMessage` and log it.
+
+// Replace the word "thinking" to "decide", and re-assign the output to the variable `newMessage` and log it.
+
+// Log all the characters from the message variable (you can user for..of loop on string)
+
+// Split all the words in message (split by " " space) and store it in a variable messageArray
+
+// Log all the words of messageArray
